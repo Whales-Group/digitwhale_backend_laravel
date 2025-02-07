c715a2f9b39375ad0f3ab0bbacc94ceee1cb4e0a
Finn Jesse Dan <83787516+Jesse-Dan@users.noreply.github.com>
2025-02-07 23:47:40 +0100

    [add]: extra success handlers

--- app/Modules/FincraModule/Handlers/HandleTransferSuccess.php
+++ app/Modules/FincraModule/Handlers/HandleTransferSuccess.php
@@ -27,12 +27,20 @@ class HandleTransferSuccess
             'balance' => $newBalance,
         ]);
 
+
         // Store the successful transaction
         $transaction = TransactionEntry::create([
             'transaction_reference' => $transactionData['reference'],
             'from_user_name' => $transactionData['customerName'],
             'from_account' => $transactionData['senderAccountNumber'] ?? 'Unknown',
-            'to_sys_account_id' => null,
+            'to_sys_account_id' => $account->id,
+            'to_user_name' => $account->user->profile_type == 'personal'
+                ? $account->user->first_name . " " . $account->user->middle_name . " " . $account->user->last_name
+                : $account->user->business_name,
+            'to_user_email' => $account->user->email,
+            'to_bank_name' => $account->service_bank,
+            'to_bank_code' => $account->service_bank,
+            'to_account_number' => $account->account_number,
             'currency' => $transactionData['sourceCurrency'],
             'amount' => $transactionData['destinationAmount'],
             'status' => 'successful',
