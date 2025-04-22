fa6b43ab4ff98972e75a5f538425b3dfabf965ec
Finn Jesse Dan <83787516+Jesse-Dan@users.noreply.github.com>
2025-04-22 13:26:26 +0100

    Update TransferService.php

--- app/Modules/TransferModule/Services/TransferService.php
+++ app/Modules/TransferModule/Services/TransferService.php
@@ -1,4 +1,4 @@
-a<?php
+<?php
 
 namespace App\Modules\TransferModule\Services;
 
@@ -206,7 +206,7 @@ class TransferService
         ]), true);
 
         return [
-            'amount' => $request->amount,
+            'amount' => (int) $request->amount - (int) $validationResponse['charge'],
             'charge' => $validationResponse['charge'],
         ];
     }
